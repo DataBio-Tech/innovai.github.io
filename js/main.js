a659a8e502833af470bd84aab09364371f50fa7f
Felipe A. Millacura <millacurafa@databio.tech>
2021-07-29 10:40:03 -0400

    adds images

--- js/main.js
+++ js/main.js
@@ -189,7 +189,7 @@
 	/*----------------------------------------------------*/
 	/* Final Countdown Settings
 	------------------------------------------------------ */
-	var finalDate = '2021/02/28';
+	var finalDate = '2021/12/31';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
